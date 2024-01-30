13bc9c19dd09d18907488b94e54a9733b213b676
Danilo Ewerton <danilo.ewe@gmail.com>
2024-01-30 09:01:16 -0300

    criando api de produtos

---
+++ fake-products-api/src/main/java/br/com/daniloewerton/FakeProductsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniloewerton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeProductsApiApplication.class, args);
+	}
+
+}
---
+++ fake-products-api/src/test/java/br/com/daniloewerton/FakeProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniloewerton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
